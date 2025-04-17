4782a95a3bcbb46e856412c475be1590a43f8c76
YaroslavStepanovLENDR <yaroslav.stepanovrud@gmail.com>
2025-04-17 20:03:38 +0300

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import UploadImage from './UploadImage'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<UploadImage />)
