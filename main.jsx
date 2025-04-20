5d8399a8b76482d7e93c95f2d0a9a87d2cdfc344
YaroslavStepanovLENDR <yaroslav.stepanovrud@gmail.com>
2025-04-20 13:15:37 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -2,4 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import UploadImage from './UploadImage'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<UploadImage />)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <UploadImage />
+  </React.StrictMode>
+)
