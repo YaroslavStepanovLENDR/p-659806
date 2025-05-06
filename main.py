3b823cc43e41aa4a259a612bc0657026ba68d13a
YaroslavStepanovLENDR <yaroslav.stepanovrud@gmail.com>
2025-05-06 11:54:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from openai import OpenAI
-import base64
-import os
-import traceback
-import json
+import base64, os, traceback, json
 
 app = FastAPI()
 
